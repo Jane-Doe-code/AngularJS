7457575021be0be50ce4ef2ab5fd4c8e77836d42
DeepBlue <deepblue5991@gmail.com>
2024-07-22 17:00:24 +0300

    chore: Update image paths and remove unused code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'contact-us', component: ContactUsComponent }
+    ]
+  },
+  { path: 'login', component: LoginComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
+  { path: 'register', component: RegisterComponent },
+  // Add more routes here
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeLayoutComponent,
+    HomeComponent,
+    AboutComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    ContactUsComponent,
+    ResetPasswordComponent,
+    RegisterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
